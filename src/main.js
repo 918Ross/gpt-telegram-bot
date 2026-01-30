8bc2096684c648e94dbc54ecdc5ffeef42f8c22f
0xHide <danielngo2000@gmail.com>
2026-01-31 01:04:04 +0300

    cleaned

--- src/main.js
+++ src/main.js
@@ -5,33 +5,25 @@ import OpenAI from 'openai'
 import config from 'config'
 
 const GPT_MODEL = 'gpt-4o'
-const PROMPT_1 = 'Сделай выжимку и вывод. Если контекcт про экономику, дополнительно дай оценку, положительно или отрицательно это влияет на криптовалюты'
+const PROMPT_1 = 'Сделай краткую выжимку и вывод простыми словами. Если контекcт про экономику, дополнительно дай оценку, положительно или отрицательно это влияет на крипторынок и экономику США '
 const client = new OpenAI({
-    apiKey: config.get('OPENAI_API_KEY'), // This is the default and can be omitted
+    apiKey: config.get('OPENAI_API_KEY'), // Вставьте сюда апи ключ 
 });
-const bot = new Telegraf(config.get('apiKey'), {
+const bot = new Telegraf(config.get('apiKey'), {      // вместо 'apiKey' вставьте апи ключ вашего бота тг  
     handlerTimeout: Infinity
 })
-
-
 bot.on(message, async (ctx) => {
-    const userMsg = JSON.parse(JSON.stringify(ctx.message))
-    // console.log(userMsg)
     let channelMsg = ''
-
     typeof ctx.message.caption === 'string' ? channelMsg = ctx.message.caption : false
     typeof ctx.message.text === 'string' ? channelMsg = ctx.message.text : false
     const loader = new Loader(ctx)
     loader.show()
-    // ctx.reply('👀')
-
     try {
         const response = await client.responses.create({
             model: GPT_MODEL,
             instructions: PROMPT_1,
             input: channelMsg,
         })
-
         console.log('🟢 ', response.output_text, '🟢');
         ctx.reply(response.output_text, {
             reply_parameters: {
@@ -43,7 +35,6 @@ bot.on(message, async (ctx) => {
         loader.error()
         console.log(error)
     }
-
 })
 bot.launch()
 
